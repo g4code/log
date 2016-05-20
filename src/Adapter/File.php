8a9e941e042f9407874f67d587b9a78f690615c6
Drasko Gomboc <drasko.gomboc@gmail.com>
2016-05-20 15:20:11 +0200

    File adaper

--- src/Adapter/File.php
+++ src/Adapter/File.php
@@ -2,8 +2,51 @@
 
 namespace G4\Log\Adapter;
 
+use G4\Log\AdapterInterface;
+
 class File implements AdapterInterface
 {
 
+    /**
+     * @var string
+     */
+    private $filename;
+
+    /**
+     * File constructor.
+     * @param $filename
+     */
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    /**
+     * @param $data
+     */
+    public function save($data)
+    {
+        error_log("\n" . $this->format($data->getRawData()) . "\n", 3, $this->filename);
+    }
+
+    /**
+     * @param $data
+     */
+    public function saveAppend($data)
+    {
+        error_log($this->format($data->getRawData()) . "\n", 3, $this->filename);
+    }
+
+    /**
+     * @param array $rawData
+     * @return string
+     */
+    private function format(array $rawData)
+    {
+        array_walk($rawData, function(&$value, $key) {
+            $value = str_pad($key . ": ", 15) . $value;
+        });
+        return implode($rawData,"\n");
+    }
 
 }
