542b6c491d6df356101513e984f60e65ebbc9fcf
Drasko Gomboc <drasko.gomboc@gmail.com>
2016-05-27 15:23:01 +0200

    Removed data mapper dependency from log - curl solr adapter implementation

--- src/Adapter/Solr.php
+++ src/Adapter/Solr.php
@@ -7,37 +7,68 @@ use G4\Log\AdapterInterface;
 class Solr implements AdapterInterface
 {
 
-    /**
-     * @var array
-     */
-    private $params;
+    const TIMEOUT = 1;
+
+    const IDENTIFIER_KEY = 'id';
+
+    private $collection;
+
+    private $host;
+
+    private $url;
+
 
     /**
      * Solr constructor.
      * @param array $params
      */
-    public function __construct(array $params)
+    public function __construct($host, $collection)
     {
-        $this->params = $params;
+        $this->host       = $host;
+        $this->collection = $collection;
     }
 
     public function save($data)
     {
-        $this->getMapper()->update($data);
+        $this->send([$data->getRawData()]);
     }
 
     public function saveAppend($data)
     {
-        $this->getMapper()->updateAdd($data);
+        $rawData = $data->getRawData();
+        array_walk($rawData, function(&$value, $key){
+            if ($key != self::IDENTIFIER_KEY) {
+                $value = ['add' => $value];
+            }
+        });
+        $this->send([$rawData]);
     }
 
-    private function getMapper()
+    private function buildUrl()
     {
-        return new \G4\DataMapper\Mapper\Solr($this->getMapperAdapter());
+        if ($this->url === null) {
+            $this->url = join('', [
+                $this->host,
+                '/solr/',
+                $this->collection,
+                '/update/',
+            ]);
+        }
+        return $this->url;
     }
 
-    private function getMapperAdapter()
+    private function send(array $data)
     {
-        return new \G4\DataMapper\Adapter\Solr\Curl($this->params);
+        $ch = curl_init($this->buildUrl());
+        curl_setopt_array($ch, [
+            CURLOPT_POST           => 1,
+            CURLOPT_POSTFIELDS     => json_encode($data),
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_TIMEOUT        => self::TIMEOUT,
+            CURLOPT_URL            => $this->buildUrl(),
+        ]);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_exec($ch);
+        curl_close($ch);
     }
 }
