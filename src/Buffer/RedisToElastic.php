27761025e4e4fe129a360fc7d99ec7fc0553339c
Drasko Gomboc <drasko.gomboc@gmail.com>
2019-01-24 13:35:51 +0100

    Added batchsize as 3rd argument in RedisToElastic

--- src/Buffer/RedisToElastic.php
+++ src/Buffer/RedisToElastic.php
@@ -3,18 +3,31 @@
 namespace G4\Log\Buffer;
 
 use G4\Log\Adapter\ElasticsearchCurl;
+use G4\ValueObject\IntegerNumber;
 
 class RedisToElastic
 {
 
+    /**
+     * @var IntegerNumber
+     */
+    private $batchsize;
+
+    /**
+     * @var ElasticsearchCurl
+     */
     private $elasticClient;
 
+    /**
+     * @var \Redis
+     */
     private $redisClient;
 
-    public function __construct(\Redis $redisClient, ElasticsearchCurl $elasticClient)
+    public function __construct(\Redis $redisClient, ElasticsearchCurl $elasticClient, IntegerNumber $batchsize)
     {
         $this->redisClient      = $redisClient;
         $this->elasticClient    = $elasticClient;
+        $this->batchsize        = $batchsize;
     }
 
     public function transferData()
